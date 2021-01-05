743faf7059d0beccba2b0d96bd36e42c887bdc69
anylogic_국승현 <cooksh2421>
2021-01-05 18:31:15 +0900

    jpa 작업중

--- src/main/java/springboot/domain/posts/Posts.java
+++ src/main/java/springboot/domain/posts/Posts.java
@@ -51,6 +51,21 @@ public class Posts {
 * 스프링 부트 2.0에서는 GeneratedType.IDENTITY 옵션을 추가해야만 auto_increment가 된다.
 
 (4)
+@Column
+* 테이블의 칼럼을 나타내며 굳이 선언하지 않더라도 해당 클래스의 필드는 모두 칼럼이 된다.
+* 사용하는 이유는, 기본값 외에 추가로 변경이 필요한 옵션이 있으면 사용한다.
+* 문자열의 경우 VARCHAR(255)가 기본값인데, 사이즈를 500으로 늘리고 싶거나(ex:title),
+  타입을 TEXT로 변경하고 싶거나(ex: content) 등의 경우에 사용된다.
+
+(5)
+@NoArgsContructor
+* 기본 생성자 자동 추가
+* public Posts(){}와 같은 효과
+
+(6)
+@Builder
+* 해당 클래스의 빌더 패턴 클래스를 생성
+* 생성자 상단에 선언 시 생성자에 포함된
 
 
 
