7ee66c3859baa46cd15bf0d2d9617992a66294f0
anylogic_국승현 <cooksh2421>
2021-01-20 18:02:10 +0900

    시큐리티 작업중

--- src/main/java/springboot/config/auth/SecurityConfig.java
+++ src/main/java/springboot/config/auth/SecurityConfig.java
@@ -1,4 +1,31 @@
 package springboot.config.auth;
 
-public class SecurityConfig {
+import lombok.RequiredArgsConstructor;
+import org.h2.engine.Role;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity      //..(1)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().headers().frameOptions().disable()    //..(2)
+                .and()
+                    .authorizeRequests()    //..(3)
+                    .antMatchers("/","/css/**","/js/**","/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())    //..(4)
+                    .anyRequest().authenticated()   //..(5)
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")   //..(6)
+                .and()
+                    .oauth2Login()      //..(7)
+                        .userInfoEndpoint()     //..(8)
+                            .userService(customOAuth2UserService);     //..(9)
+    }
 }
--- src/main/java/springboot/domain/user/Role.java
+++ src/main/java/springboot/domain/user/Role.java
@@ -1,4 +1,25 @@
 package springboot.domain.user;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class Role {
+
+    GUEST("ROLE_GUEST", "손님"),      //..(1)
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
 }
+
+
+
+/*
+
+(1)
+스프링 시큐리티에서는 권한 코드에 항상 'ROLE_'이 앞에 있어야 한다.
+그래서 코드별 키값을 ROLE_GUEST, ROLE_USER 등으로 지정한다.
+
+*/
